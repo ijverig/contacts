2754da43089fa9eaaffaaf6fd2b2ce30a8c6b48a
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-08-12 19:28:54 -0300

    Initial Commit

---
+++ Contacts/LOLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  LOLAppDelegate.h
+//  Contacts
+//
+//  Created by ios4609 on 12/08/14.
+//  Copyright (c) 2014 Rafael França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LOLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Contacts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Contacts
+//
+//  Created by ios4609 on 12/08/14.
+//  Copyright (c) 2014 Rafael França. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "LOLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([LOLAppDelegate class]));
+    }
+}
