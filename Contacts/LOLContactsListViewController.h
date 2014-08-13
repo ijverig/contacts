3e86b5e46b64845eea878ec333a1b5dc0abe0203
Rafael Mendonça França <rafaelmfranca@gmail.com>
2014-08-13 19:39:24 -0300

    Create ContactsListViewController

---
+++ Contacts/LOLContactsListViewController.h
@@ -0,0 +1,13 @@
+//
+//  LOLContactsListViewController.h
+//  Contacts
+//
+//  Created by ios4609 on 13/08/14.
+//  Copyright (c) 2014 Rafael França. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LOLContactsListViewController : UITableViewController
+
+@end
---
+++ Contacts/LOLContactsListViewController.m
@@ -0,0 +1,13 @@
+//
+//  LOLContactsListViewController.m
+//  Contacts
+//
+//  Created by ios4609 on 13/08/14.
+//  Copyright (c) 2014 Rafael França. All rights reserved.
+//
+
+#import "LOLContactsListViewController.h"
+
+@implementation LOLContactsListViewController
+
+@end
